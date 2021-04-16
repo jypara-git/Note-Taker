4a4dec1031e5f75e8d6dc648f37480299ebd8cd2
jypara-git <zhyparaabdieva@gmail.com>
2021-04-16 10:37:28 -0700

    on button click redirect to notes route

---
+++ Develop/server.js
@@ -0,0 +1,15 @@
+// const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+// app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+app.use(express.static('public'));
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
