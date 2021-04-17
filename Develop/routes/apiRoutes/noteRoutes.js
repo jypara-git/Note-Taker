5318efa3332271b15324a481ff35bb0aa4dca988
jypara-git <zhyparaabdieva@gmail.com>
2021-04-17 11:41:17 -0700

    create save api

--- Develop/routes/apiRoutes/noteRoutes.js
+++ Develop/routes/apiRoutes/noteRoutes.js
@@ -1,8 +1,24 @@
 const router = require('express').Router();
+const fs = require('fs');
 const notes = require('../../db/db');
+const path = require('path');
+
 router.get('/notes/', (req, res) => {
     let results = notes;
     res.json(results);
+});
+function createNewNote(body, notes) {
+    const newNote = body;
+    notes.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, '../../db/db'),
+        JSON.stringify(notes)
+    )
+    return newNote;
+};
+router.post('/notes', (req, res) => {
+    const newNote = createNewNote(req.body, notes);
+    res.json(newNote);
 });
 
 module.exports = router;
